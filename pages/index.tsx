e322a55c3ce8616293fe2258f7fd0185c8f179c1
adamp <adam.poulton5@gmail.com>
2023-06-02 22:24:49 +1000

    adds background images and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
+import React, { useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
+// Components
+import { 
+  BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GradientBackgroundCon, RedSpan
+} from '@/components/QuoteGenerator/QuoteGeneratorElements';
+
+// Assets
+import CloudsThunder from '@/assets/clouds-thunder.png';
+import CloudsSun from '@/assets/clouds-sun.png';
+import { validateHeaderValue } from 'http';
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
   return (
     <>
       <Head>
@@ -15,7 +26,27 @@ export default function Home() {
       </Head>
       {/* Background*/}
       <GradientBackgroundCon>
-        
+
+        <BackgroundImage1
+          src={CloudsThunder}
+          height="300"
+          alt="cloudybackground1"
+        />
+
+        <BackgroundImage2
+          src={CloudsSun}
+          height="300"
+          alt="cloudybackground1"
+        />
+
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with <RedSpan>♥</RedSpan> by <FooterLink href="https://github.com/adam-poulton" target="_blank" rel="noopener noreferrer">Adam Poulton</FooterLink>
+          </>
+        </FooterCon>
+
       </GradientBackgroundCon>
     </>
   )
